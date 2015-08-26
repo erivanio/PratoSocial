e3918b134ce868c3045b93655c7a0a88938c8e79
Erivânio Vasconcelos <erivanio_vasconcelos@MacBook-Pro-de-Erivanio.local>
2015-08-26 00:38:19 -0300

    adicionando url dos arquivos staticos

--- pratosocial/urls.py
+++ pratosocial/urls.py
@@ -30,6 +30,7 @@ urlpatterns = patterns('',
 # Arquivos estáticos e de media
 urlpatterns += patterns('',
     url(r'^media/(.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     url(r'^ckeditor/', include('ckeditor.urls')),
 )
 
